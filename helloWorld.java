6b26f3b4f2d5da051a6ff686e6ad85e6924fa363
Jim <jimmy.a.mcrae@gmail.com>
2020-08-28 14:58:04 -0400

    Change 1

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+// Title: helloWorld.java
+// Author: Jimmy A. McRae
+// Description: The obligatory "hello world" class.
+
+public class helloWorld{
+  public static void main(String args[]){
+    System.out.println("Hello World!");
+  }
+}
+
